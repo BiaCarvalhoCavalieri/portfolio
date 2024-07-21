6ee60564d09cfdf6867e7ed2626f7124ce1a20c8
BiaCarvalhoCavalieri <bia3003carvalho@gmail.com>
2024-07-21 19:16:36 -0300

    fix: changing about me text

--- src/app/components/AboutMeSection/index.tsx
+++ src/app/components/AboutMeSection/index.tsx
@@ -7,7 +7,7 @@ export default function AboutMeSection() {
             <div className={`${styles.aboutMeContainer} container`}>
                 <h3 className={styles.aboutMeTitle}>About Me</h3>
                 <p className={styles.aboutMeText}>
-                    I'm <b>Beatriz Carvalho</b>, with 29 years old currently living in state of São Paulo - Brazil. <b>Frontend Developer</b>, working professionally as a developer since 2022.
+                    I am <b>Beatriz Carvalho</b>, with 29 years old currently living in state of São Paulo - Brazil. <b>Frontend Developer</b>, working professionally as a developer since 2022.
                     <br />
                     <br />
                     I engage in <b>e-commerce</b> projects focused on <b>website development</b> and <b>maintenance</b>. I also contribute to the development of <b>Landing Pages</b>, using technologies like NextJs, ReactJS, Tailwind, and others.
